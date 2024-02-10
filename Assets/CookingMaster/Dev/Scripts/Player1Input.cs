bc7db39d181c9d12de5062d6ca536199dbc23973
Anirudh <57024963+ILLUTE@users.noreply.github.com>
2024-02-11 01:06:37 +0530

    Added a Basic Player Controller - with Synty Simple character models added

---
+++ Assets/CookingMaster/Dev/Scripts/Player1Input.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Input : IInputInterface
+{
+    public float GetAxisHorizontal()
+    {
+        return Input.GetAxis("Horizontal");
+    }
+
+    public float GetAxisVertical()
+    {
+        return Input.GetAxis("Vertical");
+    }
+
+    public bool GetInteractButton()
+    {
+        return Input.GetKey(KeyCode.Space);
+    }
+}
---
+++ Assets/CookingMaster/Dev/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private IInputInterface currentInput;
+
+    private Vector3 movement;
+
+    [SerializeField] private CharacterController m_CharacterController;
+    [SerializeField] private float m_MoveSpeed = 5;
+    [SerializeField] private Animator m_Animator;
+
+    private float smoothTime = 0.05f;
+    private float _currentVelocity;
+    private void Awake()
+    {
+        currentInput = new Player1Input();
+    }
+
+    private void Update()
+    {
+        float Horizontal = currentInput.GetAxisHorizontal();
+        float Vertical = currentInput.GetAxisVertical();
+
+        movement = new Vector3(Horizontal, 0, Vertical);
+        movement.Normalize();
+        if (movement.magnitude > 0)
+        {
+            Rotate();
+        }
+
+        m_Animator.SetFloat("Speed_f", movement.magnitude == 0 ? 0 : 0.5f);
+    }
+
+    private void Rotate()
+    {
+        var targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _currentVelocity, smoothTime);
+        transform.rotation = Quaternion.Euler(0, angle, 0.0f);
+    }
+
+    private void FixedUpdate()
+    {
+        m_CharacterController.Move(m_MoveSpeed * Time.fixedDeltaTime * movement);
+    }
+}
